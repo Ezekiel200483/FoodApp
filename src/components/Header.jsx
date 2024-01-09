4a113d7d6658dc4df3c010cbb05114aaea92bb20
Ezekiel200483 <olopade.ezekiel1998@gmail.com>
2024-01-09 19:00:27 +0100

    continue FoodApp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import logoImg from '../assets/logo.jpg'
+import Button from './UI/Button.jsx'
 
 export default function Header() {
     return (
         <header id="main-header">
             <div id="title">
                 <img src={logoImg} alt='A restaurant'/>
-                <h1>ReactFood</h1>
+                <h1>OlaMummy</h1>
             </div>
             <nav>
-                <button>Cart (0)</button>
+                <Button textOnly>Cart (0)</Button>
             </nav>
 
         </header>
---
+++ src/store/CartContex.jsx
@@ -0,0 +1,47 @@
+import { createContext, useReducer } from "react";
+
+const CartContext = createContext({
+  item: [],
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const updatedItems = [...state.items];
+
+
+    if (existingCartItemIndex > -1) {
+        const existingItem = state.items[existingCartItemIndex]
+        const updatedItem = {
+          ...existingItem,
+          quantity: existingItem.quantity + 1
+
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+
+    } else {
+        updatedItems.push({...action.item, quantity: 1 });
+
+    }
+
+
+    return { ...state, items: updatedItems };
+
+
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+    //Remove an item from the state
+  }
+}
+
+export function CartContextProvider({ children }) {
+  useReducer(cartReducer, { items: [] });
+
+  return <CartContext.Provider>{children}</CartContext.Provider>;
+}
+export default CartContext;
